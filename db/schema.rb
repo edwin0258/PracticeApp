d8aa6d843cab48e37aef633e1c7b8c48a207789a
edwin0258 <edwin0258@gmail.com>
2015-08-12 16:49:53 -0700

    Added Relationships (followers and following)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150812191558) do
+ActiveRecord::Schema.define(version: 20150812234048) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "title"
@@ -36,6 +36,17 @@ ActiveRecord::Schema.define(version: 20150812191558) do
     t.text    "body"
     t.integer "user_id"
   end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id"
+  add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
